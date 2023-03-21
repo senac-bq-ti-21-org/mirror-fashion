c8b3ff57bd18c530b92f5389b1702c7122b64e4d
KaioHSG <kaio12hsg@gmail.com>
2023-03-21 09:16:16 -0300

    Update

---
+++ Atividade 13/js/index.js
@@ -0,0 +1,16 @@
+document.querySelector("#form-busca").onsubmit = validaBusca;
+document.querySelector("#q").onfocus = restaurarBusca;
+document.querySelector("#q").onkeypress = restaurarBusca;
+
+
+function validaBusca() {
+    if(document.querySelector("#q").value == "") {
+        document.querySelector("#q").style.background = "#ffaaaa";
+        document.querySelector("#q").placeholder = "Digite uma pesquisa";
+        return false;
+    }
+}
+function restaurarBusca() {
+    document.querySelector("#q").style.background = "#ffffff";
+    document.querySelector("#q").placeholder = "";
+}
